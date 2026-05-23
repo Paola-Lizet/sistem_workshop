3148ab916cf0f292aef4faf6b116cca4e3aa986a
Paola Lizet <pjimenez05@alumnos.uaq.mx>
2026-05-22 21:47:33 -0600

    Desarrollo de db.js e index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const morgan = require('morgan');
 const express = require("express");
 const app = express();
 const cors = require('cors');
-//Acceso a archivos 
+const path = require('path'); 
+
+// Acceso a archivos 
 const empleados = require('./routes/empleados');
 const user = require('./routes/user');
 const auth = require('./middleware/auth');
@@ -12,13 +14,19 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => res.status(200).json({ message: "Bienvenido al sistema de RH de NODE JS" }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use("/user", user);
 
 // Rutas protegidas solo para usuarios con tokens
-app.use((req, res, next) => { next(); });
-app.use("/empleados", empleados);
+app.use("/empleados", auth, empleados); 
+
+// Si entran a la raíz
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Pues dice que va jalando");
+    console.log("Pues dice que va jalando en el puerto 3000");
 });
