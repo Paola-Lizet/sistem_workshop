22d46e4ed6e3964bd7ce9a728e136dad4e1bee63
juanpamarz <jmonroy26@alumnos.uaq.mx>
2026-05-22 23:20:30 -0600

    Arreglando error en la conexión con la base de datos

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '', 
-    database: process.env.DB_NAME || 'sistema_node', 
+    database: process.env.DB_NAME || 'taller_node', 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
